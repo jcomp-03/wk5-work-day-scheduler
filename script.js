edaffc6ba4e935d263ce3dd4000d58c6e320f5e8
jcomp-03@James-Desktop <jcomp_03@yahoo.com>
2022-01-30 09:36:03 -0500

    Coded functionality for styling based on hour

--- script.js
+++ script.js
@@ -3,3 +3,31 @@
 var thisDay = moment().format("dddd, MMMM Do YYYY");
 $("#currentDay").text(thisDay);
 console.log(thisDay);
+
+
+function colorTimeBlocks() {
+    // get the current hour
+    var momentHour = moment().hour();
+    console.log(momentHour);
+
+    // compare momentHour with the time blocks by using the div id attribute
+    $(".row").each(function() {
+        var rowId = $(this).attr("id");
+        rowId = parseInt(rowId);
+        // console.log(rowId, typeof rowId);
+        
+        if (momentHour > rowId) {
+            // color the div grey
+            $(this).addClass("time-past");
+        } else if ( momentHour === rowId) {
+            // color the div red
+            $(this).addClass("time-present");
+        } else {
+            // color the div green
+            $(this).addClass("time-future");
+        }
+    });
+}
+
+// run the function colorTimeBlocks
+colorTimeBlocks();
